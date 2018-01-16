ac6d8c33e5bd9e2e950835f90ed64331cd54eb66
Romi Awasthy <romiawasthy@gmail.com>
2018-01-16 10:14:23 -0500

    Demo001 morning

--- src/app/components/sidebar/sidebar-routes.config.ts
+++ src/app/components/sidebar/sidebar-routes.config.ts
@@ -4,15 +4,15 @@ export const ROUTES: RouteInfo[] = [
      { path: 'search', title: 'Search',  icon:'search', class: '' },
      { path: 'metadata', title: 'Metadata Management',  icon:'folder', class: '' },
 
-     { path: 'dashboard', title: 'Analytical Dashboard',  icon: 'dashboard', class: '' },
 
 
-    { path: 'table-list', title: 'Classificaton',  icon:'library_books', class: '' },
+
+    { path: 'table-list', title: 'Document Classes',  icon:'library_books', class: '' },
     { path: 'user-profile', title: 'Policy Mangement',  icon:'person', class: '' },
+ { path: 'notifications', title: 'Workflows',  icon:'notifications', class: '' },
+ { path: 'dashboard', title: 'Sample Dashboard',  icon: 'dashboard', class: '' },
+    { path: 'icons', title: 'Sample Embedded UI',  icon:'bubble_chart', class: '' },
+    { path: 'maps', title: 'Sample Maps',  icon:'location_on', class: '' }
 
-
-    { path: 'icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: 'maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: 'notifications', title: 'Notifications',  icon:'notifications', class: '' }
 
 ];
